1b6436b4a18f95c6ce7f6431318710a3ad318e1a
mseppy <mseppy@gmail.com>
2020-04-25 14:51:15 -0400

    Add project files.

---
+++ DataAccess/HackerNewsAPI.cs
@@ -0,0 +1,28 @@
+﻿using Models;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class HackerNewsAPI : IRepository
+    {
+        readonly HttpClient httpClient;
+
+
+        public HackerNewsAPI(HttpClient client)
+        {
+            httpClient = client;
+        }
+
+
+        async Task<int> GetMaxId()
+        {
+            var response = await httpClient.GetAsync("maxitem.json");
+            response.EnsureSuccessStatusCode();
+
+            var responseStream = await response.Content.ReadAsStreamAsync();
+            return await JsonSerializer.DeserializeAsync<int>(responseStream);
+        }
+    }
+}
